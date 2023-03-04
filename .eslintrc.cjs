20771f012f1332c3d93aae643099534ba3270146
cllcnkrt <cllcnkrt@outlook.com>
2023-03-04 20:49:51 +0300

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "airbnb-base",
+    "airbnb-typescript",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "no-shadow": "off",
+    "@typescript-eslint/no-shadow": "off",
+    "import/no-extraneous-dependencies": "off",
+  },
+};
